6595ee113110e065a53dc01bb35ae183dc895797
Syed-Muntazir-Abbas <abbasmuntazir62@gmail.com>
2026-09-06 15:55:51 +0530

    Add files via upload

---
+++ 08-mini-context/src/App.jsx
@@ -0,0 +1,36 @@
+
+// import './App.css'
+// import Login from './components/Login'
+// import UserContextProvider from './context/UserContextProvider'
+
+// function App() {
+  
+
+//   return (
+//     <UserContextProvider>
+//     <h1>chai aur code </h1>
+//     <Login/>
+//     <profile/>
+//     </UserContextProvider>
+//   )
+// }
+
+// export default App
+import './App.css'
+import Login from './components/Login'
+import Profile from './components/Profile'
+import UserContextProvider from './context/UserContextProvider'
+
+function App() {
+  
+
+  return (
+    <UserContextProvider>
+      <h1>React with Chai and share is important</h1>
+      <Login />
+      <Profile />
+    </UserContextProvider>
+  )
+}
+
+export default App
